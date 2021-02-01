6dfb5233226a0bc342ffe69a2ea8114950b0896a
ana.letca <anadoraletca@gmail.com>
2021-02-01 13:35:55 +0200

    spring init

---
+++ licenta1/src/main/java/com/ex/licenta1/Licenta1Application.java
@@ -0,0 +1,13 @@
+package com.ex.licenta1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Licenta1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Licenta1Application.class, args);
+	}
+
+}
---
+++ licenta1/src/test/java/com/ex/licenta1/Licenta1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.licenta1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Licenta1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
